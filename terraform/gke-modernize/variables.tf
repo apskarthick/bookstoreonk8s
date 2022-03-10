0871e6321e096c3fd7bf152e3330d2c71e739a29
apskarthick <aps.karthick@gmail.com>
2022-03-10 01:33:25 +0000

    restructure directories

---
+++ terraform/gke-modernize/variables.tf
@@ -0,0 +1,17 @@
+variable "clsname" {
+  type = string
+  default = "bookstore-prod"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+variable "project" {
+ type= string 
+ default = "ipl2021-342414"
+}
+variable "nodepool" {
+ type= string 
+ default = "bookstore-prod-nodepool"
+}
